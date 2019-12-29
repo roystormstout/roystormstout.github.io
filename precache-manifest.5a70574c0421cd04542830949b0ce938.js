2c5de021ea99d85a19416a6b9e2a8feb6cc86df3
Lingfeng Guo <lig012@ucsd.edu>
2019-12-28 19:38:49 -0800

    Updates

---
+++ precache-manifest.5a70574c0421cd04542830949b0ce938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90400cdd27a508401a71506ef25e5a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c11dbf569d091fdccdb7",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "129b020f9c41d3361aa1",
+    "url": "/static/js/2.238aedf1.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/static/js/2.238aedf1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c11dbf569d091fdccdb7",
+    "url": "/static/js/main.16c0029f.chunk.js"
+  },
+  {
+    "revision": "1e776d5e8ef95b73445a",
+    "url": "/static/js/runtime-main.641eebc7.js"
+  }
+]);
